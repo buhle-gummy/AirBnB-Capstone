e353e954ef3ebd1b6b12de6ee0eb26df579dc52b
Buhle Mhlanga <buhlemhlanga2003@gmail.com>
2026-09-14 23:59:11 +0200

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/users/login", {
+      const response = await fetch("https://airbnb-capstone-server.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
