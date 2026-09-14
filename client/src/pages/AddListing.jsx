14c0b2e30357644c091193cbbd53ee20b89561d6
Buhle Mhlanga <buhlemhlanga2003@gmail.com>
2026-09-14 23:46:56 +0200

    Update AddListing.jsx

--- client/src/pages/AddListing.jsx
+++ client/src/pages/AddListing.jsx
@@ -37,7 +37,7 @@ function AddListing() {
     try {
       const token = localStorage.getItem("token");
       if (!token) throw new Error("You are not logged in.");
-      const response = await fetch("http://localhost:5000/api/accommodations", {
+      const response = await fetch("https://airbnb-capstone-server.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
